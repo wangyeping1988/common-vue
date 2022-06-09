7950dc76e201ad14a3008e778b6c0c5a0be2dd1d
Wang Ye Ping <wangyeping1988@outlook.com>
2022-06-09 20:18:24 +0800

    include and configure element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/es/components/message/style/css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(ElementPlus, { size: 'small', zIndex: 3000 })
+    .mount('#app')
